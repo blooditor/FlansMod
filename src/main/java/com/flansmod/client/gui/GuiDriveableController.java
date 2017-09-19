2c139d15b6f53fb1e4bc98b753690e0654d10a0e
tim-rue <tim.ruecker98@web.de>
2017-09-19 23:47:07 +0200

    Fire forge KeyPressed event for other mods while being inside driveable

--- src/main/java/com/flansmod/client/gui/GuiDriveableController.java
+++ src/main/java/com/flansmod/client/gui/GuiDriveableController.java
@@ -14,6 +14,7 @@ import net.minecraft.client.gui.GuiChat;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
 
 public class GuiDriveableController extends GuiScreen 
 {
@@ -134,6 +135,8 @@ public class GuiDriveableController extends GuiScreen
 		{
 			FlansModClient.reloadModels(false);
 		}
+		//fires click event for other mods
+		net.minecraftforge.fml.common.FMLCommonHandler.instance().bus().post(new InputEvent.KeyInputEvent());
 	}
 	
 	@Override
